7a611dea25816c226a079d58b137174fb51b0698
diddyholz <47055527+diddyholz@users.noreply.github.com>
2023-11-01 12:12:06 +0100

    Fixed tests

--- src/test_01.sh
+++ src/test_01.sh
@@ -3,4 +3,11 @@
 # Usage: test_01.sh [user password]
 # Subscribes to MQTT topic "test" on local broker
 
-mosquitto_sub -h localhost -t test -u $1 -P $2
+auth=""
+
+if [[ -n "$1" && -n "$2" ]]
+then
+    auth="-u \"$1\" -P \"$2\""
+fi
+
+mosquitto_sub -h localhost -t test $auth
--- src/test_02.sh
+++ src/test_02.sh
@@ -3,12 +3,19 @@
 # Usage: ./test_02.sh IP [user password]
 # Sends messages to MQTT Broker on topic "test"
 
-if [[ -z $1 ]]
+if [[ -z "$1" ]]
 then
     echo "Usage: $0 IP [user password]"
 fi
 
+auth=""
+
+if [[ -n "$2" && -n "$3" ]]
+then
+    auth="-u \"$2\" -P \"$3\""
+fi
+
 while true:
-    mosquitto_pub -h $1 -t test -m "TEST" -u $2 -P $3
+    mosquitto_pub -h $1 -t test -m "TEST" $auth
     sleep 1
 done
